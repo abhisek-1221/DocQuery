9c8e2c72a5e083ef953d7717670e87083203a1ba
abhisek1221 <cherrymx317@gmail.com>
2024-07-10 04:40:38 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-          <h1 className="mr-3 text-5xl font-semibold text-white">Converse with <span className="text-purple-300">Docs</span></h1>
+          <h1 className="mr-3 text-5xl font-semibold text-white">AI Intelligence with <span className="text-purple-300">NoteTaking</span></h1>
           <UserButton afterSignOutUrl="/" />
           </div>
 
