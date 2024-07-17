7237d349ba1f998ab2921fe2348f19e8b3dab0c8
abhisek1221 <cherrymx317@gmail.com>
2024-07-17 12:50:17 +0530

    Saving Chat Component Logs to DB

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,7 +2,7 @@ import { getContext } from '@/lib/context';
 import { db } from '@/lib/db';
 import { createOpenAI } from '@ai-sdk/openai';
 import { StreamingTextResponse, streamText, StreamData, Message } from 'ai';
-import { chats } from '@/lib/db/schema';
+import { chats, messages as _messages } from '@/lib/db/schema';
 import { eq } from 'drizzle-orm';
 import { NextResponse } from 'next/server';
 
@@ -51,9 +51,22 @@ export async function POST(req:Request){
         const data = new StreamData();
         
         const stream = response.toAIStream({
-            onCompletion(_){
-                data.close();
-            }
+            onStart: async () => {
+                // save user message into db
+                await db.insert(_messages).values({
+                  chatId,
+                  content: lastMessage.content,
+                  role: "user",
+                });
+              },
+              onCompletion: async (completion) => {
+                // save ai message into db
+                await db.insert(_messages).values({
+                  chatId,
+                  content: completion,
+                  role: "system",
+                });
+              },
             });
         return new StreamingTextResponse(stream, {}, data);
     } catch (error) {
--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -2,14 +2,22 @@ import React from 'react'
 import { Message } from 'ai/react'
 import { messages } from '@/lib/db/schema'
 import { cn } from '@/lib/utils'
+import { Loader2 } from "lucide-react";
+
 
 type Props = {
+    isLoading: boolean,
     messages: Message[]
 }
 
-const MessageList = ({messages}: Props) => {
-    console.log('messages', messages);
-    
+const MessageList = ({messages, isLoading}: Props) => {
+    if(isLoading) {
+        return (
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              <Loader2 className="w-6 h-6 animate-spin" />
+            </div>
+          );
+    }   
     if(!messages) return <></>
     return(
         <div className="flex flex-col gap-2 px-4">
@@ -22,8 +30,8 @@ const MessageList = ({messages}: Props) => {
                 })}
               >
                     <div className={
-                        cn("rounded-lg px-3 text-sm py-1 shadow-md ring-1 ring-gray-900/10", {
-                            "bg-blue-600": message.role === 'user',
+                        cn("rounded-lg px-3 text-sm py-1 shadow-md ring-1 ring-gray-900/10 bg-slate-200", {
+                            "bg-blue-600 text-white": message.role === 'user',
                             "bg-gray-200": message.role === 'assistant',
                         })
                     }>
