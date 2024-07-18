d598b3754b8a5faddc3803a140ab8e361b1c9bf6
abhisek1221 <cherrymx317@gmail.com>
2024-07-18 05:47:04 +0530

    payment subscription model integration

---
+++ src/app/api/stripe/route.ts
@@ -0,0 +1,62 @@
+import { db } from "@/lib/db";
+import { userSubscriptions } from "@/lib/db/schema";
+import { currentUser, auth } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+import { stripe } from "@/lib/stripe";
+
+const return_url = process.env.NEXT_BASE_URL + "/";
+
+export async function GET() {
+    try {
+        const {userId} = await auth();
+        const user = await currentUser();
+        (!userId || !user) && NextResponse.json({ error: "unauthorized" }, { status: 401 });
+
+        const _userSubscriptions = await db
+                .select()
+                .from(userSubscriptions)
+                .where(eq(userSubscriptions.userId, userId!));
+      if (_userSubscriptions[0] && _userSubscriptions[0].stripeCustomerId) {
+        // trying to cancel at the billing portal
+        const stripeSession = await stripe.billingPortal.sessions.create({
+          customer: _userSubscriptions[0].stripeCustomerId,
+          return_url,
+        });
+        return NextResponse.json({ url: stripeSession.url });
+      }
+  
+      // user's first time trying to subscribe
+      const stripeSession = await stripe.checkout.sessions.create({
+        success_url: return_url,
+        cancel_url: return_url,
+        payment_method_types: ["card"],
+        mode: "subscription",
+        billing_address_collection: "auto",
+        customer_email: user?.emailAddresses[0].emailAddress,
+        line_items: [
+          {
+            price_data: {
+              currency: "USD",
+              product_data: {
+                name: "EcomElite Pro",
+                description: "Unlimited chat and PDF sessions with 24*7 Dashboard access!",
+              },
+              unit_amount: 2000,
+              recurring: {
+                interval: "month",
+              },
+            },
+            quantity: 1,
+          },
+        ],
+        metadata: {
+          userId,
+        },
+      });
+      return NextResponse.json({ url: stripeSession.url });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: "internal server error" }, { status: 500 });
+    }
+}
--- src/components/ChatSideBar.tsx
+++ src/components/ChatSideBar.tsx
@@ -1,16 +1,34 @@
+"use client"
+
 import { DrizzleChat } from '@/lib/db/schema'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
 import { MessageCircle, PlusCircle } from 'lucide-react'
 import { cn } from '@/lib/utils'
+import axios from 'axios'
 
 type Props = {
     chats: DrizzleChat[],
     chatId: number,
 }
 
+
 const ChatSideBar = (props: Props) => {
+    const [loading, setLoading] = React.useState(false);
+    const handleSubscription = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get('/api/stripe');
+            window.location.href = response.data.url;
+        } catch (error) {
+            console.log(error);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+
   return (
     <div className='w-full h-screen p-4 text-gray-200 bg-transparent'>
         <Link href='/'>
@@ -35,6 +53,11 @@ const ChatSideBar = (props: Props) => {
                             <Link href='/'>Home</Link>
                             <Link href='/'>Source</Link>
                         </div>
+                        <Button className='mt-2 text-white bg-blue-600' 
+                        disabled={loading}
+                        onClick={handleSubscription}>
+                            Upgrade to Pro
+                        </Button>
                     </div>
                 </Link>
             ))}
