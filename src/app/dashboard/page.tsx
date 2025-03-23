9b7c26834f275e4dd51f728f23222f9812d96148
abhisek1221 <cherrymx317@gmail.com>
2025-03-24 03:50:46 +0530

    Interactive chart Area (Sample Data)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import { AppSidebar } from "@/components/dashboard/app-sidebar";
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { ChartAreaInteractive } from "@/components/dashboard/chat-area-interactive";
 import { DataTable } from "@/components/dashboard/data-table";
-import { SectionCards } from "@/components/section-cards";
+import { SectionCards } from "@/components/dashboard/section-cards";
 import { SiteHeader } from "@/components/dashboard/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
