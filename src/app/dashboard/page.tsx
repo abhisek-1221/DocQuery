57494976eb8e4f51ce43f89e2e16ede291a57ae0
abhisek1221 <cherrymx317@gmail.com>
2025-03-24 03:44:19 +0530

    Tanstack Data table + DnD Kit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
-import { AppSidebar } from "@/components/app-sidebar";
+import { AppSidebar } from "@/components/dashboard/app-sidebar";
 import { ChartAreaInteractive } from "@/components/chart-area-interactive";
-import { DataTable } from "@/components/data-table";
+import { DataTable } from "@/components/dashboard/data-table";
 import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/dashboard/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
