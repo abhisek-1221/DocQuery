f9c8d4fde432c92a0bae3b6d009e4862fc175cf2
abhisek1221 <cherrymx317@gmail.com>
2025-03-24 04:43:26 +0530

    UI fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,66 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { cookies } from "next/headers";
+import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import Providers from "@/components/Providers";
-import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@/components/themeprovider";
+import { ActiveThemeProvider } from "@/components/dashboard/active-theme";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "ThriftWise",
-  description: "RAG based Ecommerce search engine LLM",
+const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
 };
 
-export default function RootLayout({
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata: Metadata = {
+  title: "Thritfwise",
+  description: "Ecommerce Web Search SaaS Engine",
+};
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const activeThemeValue = cookieStore.get("active_theme")?.value;
+  const isScaled = activeThemeValue?.endsWith("-scaled");
+
   return (
-
-    <ClerkProvider>
-      <Providers>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-        <Toaster />
-      </html>
-      </Providers>
-    </ClerkProvider>
-
-   
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "bg-background overscroll-none font-sans antialiased",
+          activeThemeValue ? `theme-${activeThemeValue}` : "",
+          isScaled ? "theme-scaled" : ""
+        )}
+      >
+        <ClerkProvider>
+          <Providers>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          enableColorScheme
+        >
+          <ActiveThemeProvider initialTheme={activeThemeValue}>
+            {children}
+          </ActiveThemeProvider>
+        </ThemeProvider>
+        </Providers>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
--- src/components/dashboard/chat-area-interactive.tsx
+++ src/components/dashboard/chat-area-interactive.tsx
@@ -218,24 +218,24 @@ export function ChartAreaInteractive() {
               <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
                 <stop
                   offset="5%"
-                  stopColor="var(--color-desktop)"
+                  stopColor="red"
                   stopOpacity={1.0}
                 />
                 <stop
                   offset="95%"
-                  stopColor="var(--color-desktop)"
+                  stopColor="yellow"
                   stopOpacity={0.1}
                 />
               </linearGradient>
               <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
                 <stop
                   offset="5%"
-                  stopColor="var(--color-mobile)"
+                  stopColor="green"
                   stopOpacity={0.8}
                 />
                 <stop
                   offset="95%"
-                  stopColor="var(--color-mobile)"
+                  stopColor="yellow"
                   stopOpacity={0.1}
                 />
               </linearGradient>
