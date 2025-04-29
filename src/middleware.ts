3d46c71e68afdb35016c20662ab3b9e62cd1a0ff
abhisek1221 <cherrymx317@gmail.com>
2025-04-29 17:31:32 +0530

    Fix: Bump v-14.2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,22 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-    publicRoutes: ['/', '/api/webhook','/websearch' ],
-    ignoredRoutes:['/arch', '/api/chat']
+    publicRoutes: [
+        '/',
+        '/api/webhook',
+        '/websearch',
+        '/sign-in',
+        '/sign-up',
+    ],
+    ignoredRoutes: [
+        '/arch',
+        '/api/chat'
+    ]
 });
  
 export const config = {
-  matcher: [
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    "/(api|trpc)(.*)"
-  ]
+    matcher: [
+        "/((?!.+\\.[\\w]+$|_next).*)",
+        "/(api|trpc)(.*)"
+    ]
 };
